38372dffc337129034c7bcfd3ed672ffcddf3362
Ahmed Refai <refai241@gmail.com>
2018-03-06 02:48:00 -0800

    added timed poll functionality in readScript.js, started working on writing to bc in testscript.js (still not functional)

---
+++ Blockchain Project/readScript.js
@@ -0,0 +1,328 @@
+/*
+A script that returns some filtered events from an Ethereum smart contract.
+Your contract will require a solidity event and it will need to be triggered at least once before you run the script.
+For an explanation of this code, navigate to the wiki https://github.com/ThatOtherZach/Web3-by-Example/wiki/Getting-Smart-Contract-Events
+*/
+
+// Add the web3 node module
+var Web3 = require('web3');
+
+// Add the cron node module. allows scheduling of events
+var cron = require('node-cron');
+
+// Show web3 where it needs to look for the Ethereum node.
+web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/SdO4U3ydQdgK3D3eNE2Y'));
+
+// The address we want to search by.
+var addr = "0xdd1327ece57c49f9abafcc5ac478676ae1ca0762";
+
+// Show the Hash in the console.
+console.log('Contract Location: ' + addr);
+
+// Define the contract ABI
+var abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_device",
+				"type": "address"
+			},
+			{
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"name": "_deviceType",
+				"type": "string"
+			}
+		],
+		"name": "addActuator",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHumid",
+		"outputs": [
+			{
+				"name": "humid",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "kill",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "actuators",
+		"outputs": [
+			{
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"name": "deviceType",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "temp",
+				"type": "uint256"
+			}
+		],
+		"name": "addTemp",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "humid",
+				"type": "uint256"
+			}
+		],
+		"name": "addHumid",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getTemp",
+		"outputs": [
+			{
+				"name": "temp",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "safetyCheck",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "sensors",
+		"outputs": [
+			{
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"name": "deviceType",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_device",
+				"type": "address"
+			},
+			{
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"name": "_deviceType",
+				"type": "string"
+			}
+		],
+		"name": "addSensor",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "message",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "temp",
+				"type": "uint256"
+			}
+		],
+		"name": "tempAlert",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "message",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "humid",
+				"type": "uint256"
+			}
+		],
+		"name": "humidAlert",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "temp",
+				"type": "uint256"
+			}
+		],
+		"name": "tempUpdate",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "temp",
+				"type": "uint256"
+			}
+		],
+		"name": "humidUpdate",
+		"type": "event"
+	}
+];
+
+// Define the contract ABI and Address
+var contract = new web3.eth.Contract(abi, '0xdd1327ece57c49f9abafcc5ac478676ae1ca0762');
+
+// Interval at which you want to read and display the temperature
+var interval = 5;
+
+// Fun console text, you can ignore this.
+console.log('-----------------------------------');
+console.log('Reading Temperature Every ' + interval + ' Seconds');
+console.log('-----------------------------------');
+
+// Search the contract events for the hash in the event logs and show matching events.
+// contract.getPastEvents('Event1', {
+	// filter: {_from: addr},
+	// fromBlock: 0,
+	// toBlock: 'latest'
+	// }, function(error, events){
+		// console.log(events);
+		// for (i=0; i<events.length; i++) {
+			// var eventObj = events[i];
+			// console.log('Address: ' + eventObj.returnValues._from);
+			// console.log('Greeting: ' + web3.utils.hexToAscii(eventObj.returnValues._greeting));
+		// }
+// });
+// console.log(contract.options.jsonInterface);
+// contract.methods.getTemp().call();
+
+var cronJob = cron.schedule('*/'+interval+' * * * * *', function(){
+	readTemp();
+}) 
+
+
+function readTemp(){
+	web3.eth.call({
+    	//to: '0xdd1327ece57c49f9abafcc5ac478676ae1ca0762',
+    	to: addr,
+    	data: contract.methods.getTemp().encodeABI()
+	}).then(console.log)
+}
+
+
---
+++ Blockchain Project/testscript.js
@@ -0,0 +1,353 @@
+
+//CRON TIMING  
+/****************************************************************************
+var cron = require('node-cron');
+
+var cronJob = cron.schedule("* * * * * *", function(){
+	console.log("hello\n")
+}) 
+
+// function readTemp(){
+// 	web3.eth.call({
+//     	//to: '0xdd1327ece57c49f9abafcc5ac478676ae1ca0762',
+//     	to: addr,
+//     	data: contract.methods.getTemp().encodeABI()
+// 	}).then(console.log)
+// }
+*******************************************************************************/
+
+// CONTRACT TESTING
+/*
+A script that returns some filtered events from an Ethereum smart contract.
+Your contract will require a solidity event and it will need to be triggered at least once before you run the script.
+For an explanation of this code, navigate to the wiki https://github.com/ThatOtherZach/Web3-by-Example/wiki/Getting-Smart-Contract-Events
+*/
+
+// Add the web3 node module
+var Web3 = require('web3');
+
+// transaction dependencies
+var CryptoJS = require('crypto-js')  
+
+myPrivateKey = "xxxxxxxxxxxxxxxxxxxx"
+account = "0x667C93482CFFaEe41C466f7C1B96FE0525eC1068"
+var privateKey = new Buffer(myPrivateKey, 'hex')
+
+// Show web3 where it needs to look for the Ethereum node.
+web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/SdO4U3ydQdgK3D3eNE2Y'));
+
+// The address we want to search by.
+var addr = "0xdd1327ece57c49f9abafcc5ac478676ae1ca0762";
+
+// Show the Hash in the console.
+console.log('Contract Location: ' + addr);
+
+// Define the contract ABI
+var abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_device",
+				"type": "address"
+			},
+			{
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"name": "_deviceType",
+				"type": "string"
+			}
+		],
+		"name": "addActuator",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHumid",
+		"outputs": [
+			{
+				"name": "humid",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "kill",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "actuators",
+		"outputs": [
+			{
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"name": "deviceType",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "temp",
+				"type": "uint256"
+			}
+		],
+		"name": "addTemp",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "humid",
+				"type": "uint256"
+			}
+		],
+		"name": "addHumid",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getTemp",
+		"outputs": [
+			{
+				"name": "temp",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "safetyCheck",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "sensors",
+		"outputs": [
+			{
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"name": "deviceType",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_device",
+				"type": "address"
+			},
+			{
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"name": "_deviceType",
+				"type": "string"
+			}
+		],
+		"name": "addSensor",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "message",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "temp",
+				"type": "uint256"
+			}
+		],
+		"name": "tempAlert",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "message",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "humid",
+				"type": "uint256"
+			}
+		],
+		"name": "humidAlert",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "temp",
+				"type": "uint256"
+			}
+		],
+		"name": "tempUpdate",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "temp",
+				"type": "uint256"
+			}
+		],
+		"name": "humidUpdate",
+		"type": "event"
+	}
+];
+
+// Define the contract ABI and Address
+var contract = new web3.eth.Contract(abi, '0xdd1327ece57c49f9abafcc5ac478676ae1ca0762');
+
+// Fun console text, you can ignore this.
+console.log('-----------------------------------');
+console.log('Smart Contract Execution');
+console.log('-----------------------------------');
+
+web3.eth.defaultAccount = "0x667C93482CFFaEe41C466f7C1B96FE0525eC1068";
+
+
+
+
+var functionName = 'addTemp'  
+var types = ['uint']  
+var args = [0]  
+var fullName = functionName + '(' + types.join() + ')'  
+var signature = CryptoJS.SHA3(fullName,{outputLength:256}).toString(CryptoJS.enc.Hex).slice(0, 8)  
+var dataHex = signature + web3.eth.abi.encodeParameters(types, args)  
+var data = '0x'+dataHex  
+
+var nonce = web3.utils.toHex(web3.eth.getTransactionCount(account))  
+var gasPrice = web3.utils.toHex(web3.eth.gasPrice)  
+var gasLimitHex = web3.utils.toHex(300000)
+var rawTx = { 'nonce': nonce, 'gasPrice': gasPrice, 'gasLimit': gasLimitHex, 'from': account, 'to': addr, 'data': data}  
+var tx = new Tx(rawTx)  
+tx.sign(privateKey)  
+var serializedTx = '0x'+tx.serialize().toString('hex')  
+web3.eth.sendRawTransaction(serializedTx, function(err, txHash){ console.log(err, txHash) })
+
+//console.log(web3.eth.defaultAccount);
+
+// web3.eth.sendTransaction({
+//     //to: '0xdd1327ece57c49f9abafcc5ac478676ae1ca0762',
+//     to: addr,
+//     data: contract.methods.addTemp(0).encodeABI()
+// }).then(function(receipt){
+
+// });
+
+//contract.methods.addTemp(0).send({from: "0x667C93482CFFaEe41C466f7C1B96FE0525eC1068"});
+
+// END CONTRACT TESTING
