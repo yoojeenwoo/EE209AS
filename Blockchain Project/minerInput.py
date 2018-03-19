d1f9375e7ebc94560549ff9386b5be0a84e20ed2
yoojeenwoo <eugenewu0@gmail.com>
2018-03-19 15:03:05 -0700

    miner js file and miner input file

---
+++ Blockchain Project/minerInput.py
@@ -0,0 +1,21 @@
+import urllib.request
+import time
+
+#Change this address to your settings
+device_links = ["http://192.168.0.20:8080"]
+numDevices = 1
+
+while True:
+    for i in range(0,numDevices):
+        f = urllib.request.urlopen(device_links[i])
+        myfile = f.read()
+        # date = myfile.split(" ")
+        mystr = ""
+        for line in myfile:
+            mystr = mystr + chr(line)
+    
+        f = open("sensorRequests" + str(i) + ".txt","w+")
+        f.write(mystr.strip())
+        f.close()
+        print(mystr)
+        time.sleep(5)
