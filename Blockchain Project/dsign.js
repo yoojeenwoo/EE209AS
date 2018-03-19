ce2a99e3dc96302fad70e2a3d2e0b6919a8ab9ae
Ahmed Refai <refai241@gmail.com>
2018-03-19 15:05:56 -0700

    unlock account script. digital signature example

---
+++ Blockchain Project/dsign.js
@@ -0,0 +1,48 @@
+
+var crypto = require('crypto');
+
+const fs = require('fs')
+
+const privateKey = fs.readFileSync('/home/refai/Desktop/private.pem', 'utf-8')
+const publicKey = fs.readFileSync('/home/refai/Desktop/public.pem', 'utf-8')
+//const message = fs.readFileSync('message.txt', 'utf-8')
+
+//----------------------------------------------------------------//
+
+const signer = crypto.createSign('sha256');
+const message = "some data to sign";
+
+signer.update(message);
+signer.end();
+
+//const privateKey = diffHell.getPrivateKey('hex');
+
+const signature = signer.sign(privateKey);
+const signature_hex = signature.toString('hex')
+
+//var signed = sign.sign(privateKey, 'hex');
+//console.log(signed);
+// Prints: the calculated signature using the specified private key and
+// SHA-256. For RSA keys, the algorithm is RSASSA-PKCS1-v1_5 (see padding
+// parameter below for RSASSA-PSS). For EC keys, the algorithm is ECDSA.
+
+
+//-----------------------------------------------------------------//
+
+
+const verifier = crypto.createVerify('sha256');
+
+verifier.update(message);
+verifier.end();
+
+//const publicKey = diffHell.getPublicKey('hex');
+const verified = verifier.verify(publicKey, signature);
+
+console.log(JSON.stringify({
+	message: message,
+	signature: signature_hex,
+	verified: verified,
+}, null, 2));
+//const signature = signed;
+//console.log(verify.verify(publicKey, signature));
+// Prints: true or false
